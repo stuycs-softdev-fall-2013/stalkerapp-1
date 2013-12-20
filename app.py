73f43bb4a0d5c8ca1244dd6fd30cd89558b4ed45
Mike Zamansky <zamansky@gmail.com>
2013-12-19 19:49:58 -0500

    added base and login templates
    / redirects to the login page

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, redirect, url_for
+import json
 
 app=Flask(__name__)
 
 
 
+@app.route("/loginform",methods=['GET','POST'])
+def loginform():
+    if request.method=='GET':
+        print request.args.get('nextpage')
+        return render_template("login.html")
+    else:
+        return "DOING THE LOGIN"
+
+@app.route("/login")
+def login():
+    user = request.args.get('username',"")
+    pw = request.args.get("password","")
+
+    if user=="z" and pw=="z":
+        session['user']='z'
+        return json.dumps({'user':"z"})
+    return json.dumps({'user':""})
+
+@app.route("/register")
+def register():
+    return "REGISTER PAGE"
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    session['nextpage']=request.path
+    print request.path
+    return redirect(url_for('loginform',nextpage=request.path))
 
 
+
+@app.route("/f")
+def f():
+    return render_template("base.html")
 if __name__=="__main__":
+    app.secret_key = "the secret key"
     app.debug=True
-    app.run(host="0.0.0.0",port=5000);
+    app.run(host="0.0.0.0",port=5000)
